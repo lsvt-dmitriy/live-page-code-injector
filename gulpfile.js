c20acc22ec7d18eff99e9e53e442490ff78f48fa
Dmitriy Marchenko <uatari@gmail.com>
2020-08-18 12:51:57 -0400

    Add multi-folder project support

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { watch, series, src, dest } = require('gulp');
+const { watch, series, src, dest, lastRun } = require('gulp');
 const fs = require('fs');
 const htmlmin = require('gulp-htmlmin');
 const EventEmitter = require('events');
@@ -8,27 +8,75 @@ const server = require('./server');
 const through = require('through2');
 const path = require('path');
 const replace = require('gulp-replace');
-
+const tap = require('gulp-tap');
+const minifyInline = require('gulp-minify-inline');
 const port = 3005;
 const paths = {
-	project: `src/project`,
-	cfm: `index.cfm`,
-	css: `styles/styles.css`,
+	project: process.argv.indexOf('--project') == -1 ? `src/project` : process.argv[process.argv.indexOf('--project') + 1],
+	html: `**/*.html`,
+	cfm: `**/*.cfm`,
+	css: `**/*.css`,
 	template:`src/preview/index.html`
 }
 
 paths.project = fs.realpathSync(paths.project);
-paths.css = path.resolve(paths.project, paths.css);
-paths.cfm = path.resolve(paths.project, paths.cfm);
 paths.template = path.resolve(paths.template);
+paths.cfm = `${paths.project}/${paths.cfm}`;
+paths.css = `${paths.project}/${paths.css}`;
+paths.html = `${paths.project}/${paths.html}`;
 
-const build = (done) => {
-	let cfm = fs.readFileSync(paths.cfm, 'utf8');
-	let css = fs.readFileSync(paths.css, 'utf8');
-	src(paths.template)
-		.pipe(replace(`<!-- HTML -->`, cfm))
-	    .pipe(replace(/<link rel="stylesheets" href="styles\/styles\.css" \/>/, `<style scoped>${css}</style>`))
-		.pipe(htmlmin({ collapseWhitespace: true })).on('error', function(error) {
+const buildSubDir = () => {
+	// return src(paths.css, { since: lastRun(css) })
+	console.log(`buildSubDir:`);
+	return src([paths.css, paths.cfm, paths.html], { since: new Date().getTime() - 1000 })
+	.pipe(tap((file, t) => {
+		let folderPath = path.dirname(file.path);
+		if (path.extname(file.path).substr(1) == 'css') {
+			folderPath = path.resolve(path.dirname(file.path), '..');
+		}
+		return src(folderPath).pipe(tap(build))
+	}));
+}
+
+
+const build = async (file, t) => {
+	console.log(`build: ${file.path}`);
+
+	let getStreamContent = (file) => {
+		return file.contents.toString('utf8')
+	}
+
+	let cfm = new Promise((resolve, reject) => {
+		let path = file.path + '/*.cfm';
+		src(path)
+		.pipe(tap((file, t) => {
+			resolve(file);
+		}))
+	})
+
+	let html = new Promise((resolve, reject) => {
+		let path = file.path + '/*.html';
+		src(path)
+		.pipe(tap((file, t) => {
+			resolve(file);
+		}))
+	})
+
+	let styles = new Promise((resolve, reject) => {
+		src(file.path + '/**/*.css')
+		.pipe(tap((file, t) => {
+			resolve(file);
+		}))
+	}).then(getStreamContent)
+
+	let code = Promise.race([cfm, html]).then(getStreamContent);
+
+	let results = await Promise.all([code, styles]);
+
+	return src(paths.template)
+		.pipe(replace(`<!-- HTML -->`, results[0]))
+	    .pipe(replace(/<!-- CSS -->/, `<style scoped>${results[1]}</style>`))
+		.pipe(minifyInline({ collapseWhitespace: true })).on('error', function(error) {
 			let msg = `Invalid HTML`;
 			notifier.notify(msg);
 			return done(msg);
@@ -42,10 +90,9 @@ const build = (done) => {
 			ee.emit(`update`, {html});
 			return cb();
 		}));
-	done();
 }
 
 exports.default = () => {
 	server({port, ee, notifier});
-	watch([paths.cfm, paths.css], build);
+	watch([paths.cfm, paths.html, paths.css], buildSubDir);
 }
--- src/project/index.cfm
+++
@@ -1,5 +0,0 @@
-<!-- selector: ".site-page" -->
-<link rel="stylesheets" href="styles/styles.css" />
-<div>
-  <h2>Hello world!</h2>
-</div>
